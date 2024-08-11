fbce9e2497eb8e39382a71cb381e2a774b80157e
Wil220 <wilker.silva0714@gmail.com>
2024-08-11 05:11:10 -0300

    adicionando o NavBar

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -1,38 +1,35 @@
 import { styled } from "@mui/material"
 import { ReactNode } from "react"
-import theme from "../../theme"
 
-interface StyledButtonProps{
+interface StyledButtonProps {
     children: ReactNode
 }
 
-const StyledButton: React.FC<StyledButtonProps> = ({ children }) =>{
+const StyledButton: React.FC<StyledButtonProps> = ({ children }) => {
 
-
-
-    const StyledButton = styled("button")(()=> ({
-       backgroundColor: "transparent",
-       border: "1px solid ${theme.palette.primary.contraText}",
-       borderRadius: "3px",
-       padding: "5px 15px",
-       width: "100%",
-       color: theme.palette.primary.contrastText,
-       display:"inline-flex",
-       alignItems: "center",
-       justifyContent: "center",
-       gap: "10px",
-       '&:hover':{
-        backgroundColor: theme.palette.secondary.main
-       }
+    const StyledButton = styled("button")(({ theme }) => ({
+        backgroundColor: "transparent",
+        border: `1px solid ${theme.palette.primary.contrastText}`,
+        borderRadius: "3px",
+        padding: "5px 15px",
+        width: "100%",
+        color: theme.palette.primary.contrastText,
+        display: "inline-flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "10px",
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.light
+        }
     }))
 
     return (
-      <>
-      <StyledButton>
-        {children}
-      </StyledButton>
-      </>
+        <>
+            <StyledButton>
+                {children}
+            </StyledButton>
+        </>
     )
-  }
-  
-  export default StyledButton
+}
+
+export default StyledButton
